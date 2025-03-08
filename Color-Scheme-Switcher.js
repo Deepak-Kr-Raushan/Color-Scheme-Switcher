5b150581750d83904ef629f1b0aa5247c5838c25
Deepak-Kr-Raushan <deepakraushan901@gmail.com>
2025-03-08 17:17:28 +0530

    Created Color Scheme Switcher

---
+++ Color-Scheme-Switcher.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body")
+const mainDiv = document.querySelector(".main-div")
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if(e.target.id === 'grey'){
+      body.style.backgroundColor = e.target.id;
+      mainDiv.style.backgroundColor = e.target.id
+    } else if(e.target.id === 'white'){
+      body.style.backgroundColor = e.target.id;
+      mainDiv.style.backgroundColor = e.target.id
+    } else if(e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id;
+      mainDiv.style.backgroundColor = e.target.id
+    } else if(e.target.id === 'yellow'){
+      body.style.backgroundColor = e.target.id;
+      mainDiv.style.backgroundColor = e.target.id
+    } else {
+      body.style.backgroundColor = e.target.id;
+      mainDiv.style.backgroundColor = e.target.id
+    }
+  })
+})
